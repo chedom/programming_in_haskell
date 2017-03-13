70f004236d2cfeca412270cb849572675b723173
Dom <v.domanchuk@favbet.com>
2017-03-13 13:36:32 +0200

    done chapter7_voting

--- chapter7_voting.hs
+++ chapter7_voting.hs
@@ -1,3 +1,31 @@
+import Data.List
+votes :: [String]
+votes = ["Red", "Blue", "Green", "Blue", "Blue", "Red"]
+
 count :: Eq a => a -> [a] -> Int
 count x  = length . filter (== x)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : filter(/= x) (rmdups xs)
+
+result :: Ord a => [a] -> [(Int, a)]
+result xs = sort [(count v xs, v) | v <- rmdups xs]
+
+winner :: Ord a => [a] -> a
+winner = snd . last . result
+
+rmempty :: Eq a => [[a]] -> [[a]]
+rmempty = filter (/= [])
+
+elim :: Eq a => a -> [[a]] -> [[a]]
+elim x = map (filter (/=x))
+
+rank :: Ord a => [[a]] -> [a]
+rank = map snd . result . map head
+
+winner' :: Ord a => [[a]] -> a
+winner' bs = case rank (rmempty bs) of
+                 [c]-> c
+                 (c:cs) -> winner' (elim c bs)
 
