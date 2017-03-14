89fa663ec9f21d74eace8f8dbb934e7c9caade63
Dom <v.domanchuk@favbet.com>
2017-03-14 18:14:08 +0200

    init hw8

---
+++ hw8.hs
@@ -0,0 +1,11 @@
+data Nat = Zero | Succ Nat deriving Show
+
+add :: Nat -> Nat -> Nat
+add Zero y = y
+add (Succ x) y = add x (Succ y)
+
+mult :: Nat -> Nat -> Nat
+mult Zero _ = Zero
+mult _ Zero = Zero
+mult (Succ Zero) y = y
+mult (Succ x) y = add y (mult x y)
