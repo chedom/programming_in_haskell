92a628642f5610d4b8f12a90e32d65632c013a8d
Dom <v.domanchuk@favbet.com>
2017-03-14 16:09:58 +0200

    add tautology checker

--- chapter8_tautology.hs
+++ chapter8_tautology.hs
@@ -27,3 +27,27 @@ int2bin :: Int -> [Bit]
 int2bin 0 = []
 int2bin n = n `mod` 2 : int2bin (n `div` 2)
 
+--bools :: Int - [[Bool]]
+--bools n = map (reverse . map conv . make n . int2bin) range
+--    where
+--        range = [0..(2^n) - 1]
+--       make n bs = take n (bs ++ repeat 0)
+--        conv 0 = False
+--        conv 1 = True
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False:) bss ++ map(True:) bss
+    where bss = bools (n - 1)
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : filter(/= x) (rmdups xs)
+
+substs :: Prop -> Subst
+substs p = map (zip vs) (bools (length vs))
+    where vs = rmdups (vars p)
+
+isTaut :: Prop -> Bool
+isTaut p = and [eval s p | s <- subst p]
+
