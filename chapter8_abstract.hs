3b77122d4e80cf0d9426e770c6c90dfc7631b73b
Dom <v.domanchuk@favbet.com>
2017-03-15 15:42:24 +0200

    add 9 exerchise

--- chapter8_abstract.hs
+++ chapter8_abstract.hs
@@ -1,18 +1,20 @@
-data Expr = Val Int | Add Expr Expr
+data Expr = Val Int | Add Expr Expr | Mult Expr Expr
 
 type Cont = [Op]
-data Op = EVAL Expr | ADD Int
+
+data Op   = EVALA Expr | EVALM Expr | ADD Int | MULT Int
 
 eval :: Expr -> Cont -> Int
-eval (Val n) c = exec c n
-eval (Add x y) c = eval x (EVAL y : c)
+eval (Val n)    c = exec c n
+eval (Add x y)  c = eval x (EVALA y : c)
+eval (Mult x y) c = eval x (EVALM y : c)
 
 exec :: Cont -> Int -> Int
-exec [] n = n
-exec (EVAL y : c) n = eval y (ADD n : c)
-exec (ADD n : c) m = exec c (n + m)
+exec [] n           = n
+exec (EVALA y : c) n = eval y (ADD n : c)
+exec (EVALM y : c) n = eval y (MULT n : c)
+exec (ADD  n : c) m = exec c (n + m)
+exec (MULT n : c) m = exec c (n * m)
 
 value :: Expr -> Int
 value e = eval e []
-
-
