49734ebc0980c3a71a167db4e28e306c20bae6b3
dom <domanchukits@gmail.com>
2017-03-19 16:36:33 +0200

    finish chapter9

--- chapter9.hs
+++ chapter9.hs
@@ -7,10 +7,10 @@ instance Show Op where
     show Div = "/"
 
 valid :: Op -> Int -> Int -> Bool
-valid Add _ _ = True
+valid Add x y = x <=y
 valid Sub x y = x > y
-valid Mul _ _ = True
-valid Div x y = x `mod` y == 0
+valid Mul x y = x /= 1 && y /= 1 && x <= y
+valid Div x y = y /= 1 && x `mod` y == 0
 
 apply :: Op -> Int -> Int -> Int
 apply Add x y = x + y
@@ -55,8 +55,46 @@ choices = concat . map perms . subs
 solution :: Expr -> [Int] -> Int -> Bool
 solution e ns n = elem (values e) (choices ns) && eval e == [n]
 
+split :: [a] -> [([a], [a])]
+split [] = []
+split [_] = []
+split (x:xs) = ([x], xs) : map (\(a, b) -> (x:a, b)) (split xs)
 
+exprs :: [Int] -> [Expr]
+exprs [] = []
+exprs [n] = [Val n]
+exprs ns = [e  | (ls, rs) <- split ns,
+                l <- exprs ls,
+                r <- exprs rs,
+                e <- combine l r]
 
+combine :: Expr -> Expr -> [Expr]
+combine l r = [App o l r | o <- ops]
 
+ops :: [Op]
+ops = [Add, Sub, Mul, Div]
 
+solutions :: [Int] -> Int -> [Expr]
+solutions ns n = [e | ns' <-choices ns, e <-exprs ns', eval e == [n]]
+
+type Result = (Expr, Int)
+
+results :: [Int] -> [Result]
+results [] = []
+results [n] = [(Val n,n) | n > 0]
+results ns = [res | (ls, rs) <-split ns,
+                    lx <- results ls,
+                    ry <-results rs,
+                    res <- combine' lx ry]
+
+combine' :: Result -> Result -> [Result]
+combine' (l,x) (r,y) =
+    [(App o l r, apply o x y) | o <- ops, valid o x y]
+
+solutions' :: [Int] -> Int -> [Expr]
+solutions' ns n =
+    [e | ns' <- choices ns, (e, m) <-results ns', m == n]
+
+main :: IO ()
+main = print (solutions' [1,3,7,10,25,50] 765)
 
