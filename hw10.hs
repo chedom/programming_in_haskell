9a6b00feccc789e5c84f3ec819f6b7c2e72700a8
Dom <v.domanchuk@favbet.com>
2017-03-21 14:48:16 +0200

    add hw 10 4

--- hw10.hs
+++ hw10.hs
@@ -1,3 +1,18 @@
+--1
 putStr' :: String -> IO ()
 putStr' xs = sequence_ [putChar x | x <- xs]
+
+--4
+adder :: IO ()
+adder = do putStr "How many numbers?"
+           count <- getLine
+           total <- getTotal 0 (read count :: Int)
+           putStr "The total is "
+           putStrLn (show total)
+
+getTotal :: Int -> Int -> IO Int
+getTotal total 0 = return total
+getTotal total attempt = do n <- getLine
+                            getTotal (total + (read n :: Int)) (attempt - 1)
+
 
