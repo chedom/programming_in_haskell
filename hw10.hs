f4c1c9d4212346129ea8693ad2dce54b9c98499d
Dom <v.domanchuk@favbet.com>
2017-03-21 15:22:08 +0200

    add hw10 5

--- hw10.hs
+++ hw10.hs
@@ -16,3 +16,14 @@ getTotal total attempt = do n <- getLine
                             getTotal (total + (read n :: Int)) (attempt - 1)
 
 
+--5
+adder' :: IO ()
+adder' =  do putStr "How many numbers?"
+             count <- getLine
+             total <- getTotal' (read count :: Int)
+             putStr "The total is "
+             putStrLn (show ((sum . map (\x -> read x :: Int)) total))
+
+getTotal' :: Int -> IO [String]
+getTotal' n = sequence [getLine | _ <- [1..n]]
+
